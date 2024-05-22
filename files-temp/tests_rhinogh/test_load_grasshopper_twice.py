fdb90dc7db8e8fe19b6f25b61ab93209ca77a564
eirannejad <eirannejad@gmail.com>
2024-05-22 10:52:12 -0700

    misc updates

--- files-temp/tests_rhinogh/test_load_grasshopper_twice.py
+++ files-temp/tests_rhinogh/test_load_grasshopper_twice.py
@@ -3,20 +3,27 @@ import sys
 import os.path as op
 
 import System
+from System import Guid
 from System.Reflection import Assembly
 import Rhino
 from Rhino import RhinoApp
 from Rhino.PlugIns import PlugIn as P
 
-
-rhino = op.dirname(Assembly.GetExecutingAssembly().Location)
+assm = Assembly.Load("RhinoCommon")
+rhino = op.dirname(assm.Location)
 grasshopper = op.join(rhino, r"Plug-ins\Grasshopper\GrasshopperPlugin.rhp")
 print(grasshopper)
 
 res, ghId = P.LoadPlugIn(grasshopper)
+print(res, ghId)
 gh = RhinoApp.GetPlugInObject(ghId)
 print(gh)
 
 res, ghId = P.LoadPlugIn(grasshopper)
+print(res, ghId)
 gh = RhinoApp.GetPlugInObject(ghId)
+print(gh)
+
+gh_id = Guid("B45A29B1-4343-4035-989E-044E8580D9CF")
+gh = RhinoApp.GetPlugInObject(gh_id)
 print(gh)
