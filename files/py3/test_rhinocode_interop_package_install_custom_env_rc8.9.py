6c4ad2bc5f0ff81db8a66f8362c3ef0dc5d81ec3
eirannejad <eirannejad@gmail.com>
2024-05-31 11:29:02 -0700

    added python tests

--- files/py3/test_rhinocode_interop_package_install_custom_env_rc8.9.py
+++ files/py3/test_rhinocode_interop_package_install_custom_env_rc8.9.py
@@ -1,12 +1,9 @@
 #! python3
 from rhinocode import envs
 
-pkg= envs.pip_install("requests", "2.31.0")
+pkg= envs.pip_install_to("pip_install_test", "requests", "2.31.0")
 assert str(pkg) == 'requests==2.31.0 (Any)'
 assert str(pkg.Id) == 'requests'
 assert str(pkg.Version) == '2.31.0'
-
-import requests
-assert requests is not None
 
 result = True
