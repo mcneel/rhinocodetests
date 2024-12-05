0d4726318944539e1d574a80bb1465320b1b5279
eirannejad <eirannejad@gmail.com>
2024-12-05 15:39:41 -0800

    added tests

--- files-ui/test_commands_args/TestCommandArgs.cs
+++ files-ui/test_commands_args/TestCommandArgs.cs
@@ -4,7 +4,7 @@ using System;
 // special vars should be available when project is open
 // and this code belongs to the project
 // and NOT available otherwise
-Console.WriteLine(__rhino_command__ ?? "None");
+Console.WriteLine(__rhino_command__);
 Console.WriteLine(__rhino_doc__);
 Console.WriteLine(__rhino_runmode__);
 Console.WriteLine(__is_interactive__);
