92de4a779c78850d0fdde6aafa018ac63525e94a
Ehsan Iran-Nejad <eirannejad@gmail.com>
2026-01-29 10:29:29 -0800

    Added new test files

--- files-ui/test_commands_with_libs/TestC#Library.cs
+++ files-ui/test_commands_with_libs/TestC#Library.cs
@@ -2,5 +2,6 @@ using System;
 using TestAssembly.Math;
 
 var m = new DoMath();
-Console.WriteLine($"Testing C# Library: {m.Add(21, 21)}");
+var v = m.Add(21, 21);
+Console.WriteLine($"Testing C# Library: {v}");
 Console.WriteLine($"Testing C# Library: {m.Solve()}");
