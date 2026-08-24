d954bc55ec56d71b876d2c53b58166fdf487ed66
eirannejad <eirannejad@gmail.com>
2026-08-24 14:34:43 -0700

    updated tests

--- src/RhinoCodePlatform.Rhino3D.Tests/Grasshopper1_Tests_Component.cs
+++ src/RhinoCodePlatform.Rhino3D.Tests/Grasshopper1_Tests_Component.cs
@@ -2164,7 +2164,9 @@ public class Script_Instance : GH_ScriptInstance
             // https://mcneel.myjetbrains.com/youtrack/issue/RH-86062
             // 'ExpandsItems' is only written to the archive when unwrapping is off
             var defaultChunk = new GH_LooseChunk("param");
-            new GHP.Parameters.ScriptVariableParam("a").Write(defaultChunk);
+            var sv1 = new GHP.Parameters.ScriptVariableParam("a");
+            sv1.CreateAttributes();
+            sv1.Write(defaultChunk);
             Assert.IsFalse(defaultChunk.ItemExists("ExpandsItems"));
 
             // archives without the key (older archives) must default to unwrapping on
@@ -2174,7 +2176,9 @@ public class Script_Instance : GH_ScriptInstance
 
             // unwrap-off round-trips
             var offChunk = new GH_LooseChunk("param");
-            new GHP.Parameters.ScriptVariableParam("a") { ExpandsItems = false }.Write(offChunk);
+            var sv2 = new GHP.Parameters.ScriptVariableParam("a") { ExpandsItems = false };
+            sv2.CreateAttributes();
+            sv2.Write(offChunk);
             Assert.IsTrue(offChunk.ItemExists("ExpandsItems"));
 
             fresh = new GHP.Parameters.ScriptVariableParam("a");
@@ -2189,8 +2193,9 @@ public class Script_Instance : GH_ScriptInstance
             // by default a dictionary assigned to an output is unwrapped into its keys.
             // when 'Unwrap Data' is off, the dictionary must pass through as a single item.
             // python defaults to guid marshalling so this exercises the GuidMarshOut converter
-            IScriptObject script = GHP.Components.Python3Component.Create("Test", @"a = {'x': 1, 'y': 2}") as IScriptObject;
+            IScriptObject script = GHP.Components.Python3Component.Create("Test", @"a = {'x': 1, 'y': 2}");
             script.CaptureOutput = false;
+            script.MarshOutputs = true;
 
             IGH_Component component = (IGH_Component)script;
             var doc = new GH_Document();
@@ -2199,14 +2204,18 @@ public class Script_Instance : GH_ScriptInstance
 
             doc.NewSolution(expireAllObjects: true);
 
+            IList data;
             GHP.Parameters.ScriptVariableParam a_param = (GHP.Parameters.ScriptVariableParam)component.Params.Output[0];
-            Assert.AreEqual(2, a_param.VolatileData.DataCount);
+            Assert.AreEqual(1, a_param.VolatileData.PathCount);
+            data = a_param.VolatileData.get_Branch(0);
+            Assert.AreEqual(2, data.Count);
 
             a_param.ExpandsItems = false;
             doc.NewSolution(expireAllObjects: true);
 
-            Assert.AreEqual(1, a_param.VolatileData.DataCount);
-            Assert.IsNotNull(a_param.VolatileData.AllData(true).First().ScriptVariable());
+            Assert.AreEqual(1, a_param.VolatileData.PathCount);
+            data = a_param.VolatileData.get_Branch(0);
+            Assert.AreEqual(1, data.Count);
         }
 
         [Test]
@@ -2215,6 +2224,7 @@ public class Script_Instance : GH_ScriptInstance
             // https://mcneel.myjetbrains.com/youtrack/issue/RH-86062
             IScriptObject script = GHP.Components.Python3Component.Create("Test", @"a = [1, 2, 3]") as IScriptObject;
             script.CaptureOutput = false;
+            script.MarshOutputs = true;
 
             IGH_Component component = (IGH_Component)script;
             var doc = new GH_Document();
@@ -2223,14 +2233,18 @@ public class Script_Instance : GH_ScriptInstance
 
             doc.NewSolution(expireAllObjects: true);
 
+            IList data;
             GHP.Parameters.ScriptVariableParam a_param = (GHP.Parameters.ScriptVariableParam)component.Params.Output[0];
-            Assert.AreEqual(3, a_param.VolatileData.DataCount);
+            Assert.AreEqual(1, a_param.VolatileData.PathCount);
+            data = a_param.VolatileData.get_Branch(0);
+            Assert.AreEqual(3, data.Count);
 
             a_param.ExpandsItems = false;
             doc.NewSolution(expireAllObjects: true);
 
-            Assert.AreEqual(1, a_param.VolatileData.DataCount);
-            Assert.IsNotNull(a_param.VolatileData.AllData(true).First().ScriptVariable());
+            Assert.AreEqual(1, a_param.VolatileData.PathCount);
+            data = a_param.VolatileData.get_Branch(0);
+            Assert.AreEqual(1, data.Count);
         }
 
         [Test]
@@ -2267,14 +2281,18 @@ public class Script_Instance : GH_ScriptInstance
 
             doc.NewSolution(expireAllObjects: true);
 
+            IList data;
             GHP.Parameters.ScriptVariableParam a_param = (GHP.Parameters.ScriptVariableParam)component.Params.Output[0];
-            Assert.AreEqual(3, a_param.VolatileData.DataCount);
+            Assert.AreEqual(1, a_param.VolatileData.PathCount);
+            data = a_param.VolatileData.get_Branch(0);
+            Assert.AreEqual(3, data.Count);
 
             a_param.ExpandsItems = false;
             doc.NewSolution(expireAllObjects: true);
 
-            Assert.AreEqual(1, a_param.VolatileData.DataCount);
-            Assert.IsInstanceOf<IDictionary>(a_param.VolatileData.AllData(true).First().ScriptVariable());
+            Assert.AreEqual(1, a_param.VolatileData.PathCount);
+            data = a_param.VolatileData.get_Branch(0);
+            Assert.AreEqual(1, data.Count);
         }
 
         static string EnsureCRLF(string input) => input.Replace("\r\n", "\n").Replace("\r", "\n").Replace("\n", "\r\n");
